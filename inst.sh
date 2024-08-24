c8b75e5f94798b1593a3e976c409ef9746ad9533
VastSky0 <chuongsohc@gmail.com>
2024-08-24 16:00:15 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -11,6 +11,10 @@ echo -e "\e[1;33mĐang cài đặt\e[0m"
 sleep 1
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/4gAuto/raw/main/4gauto.zip && unzip 4gauto.zip && chmod a+x *  
 clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/VastSky0/4gAuto/raw/main/t1 && mv t1 motd
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/VastSky0/4gAuto/raw/main/l1 && chmod +x * && mv l1 login
+clear
 sleep 1
 echo -e "\e[1;32mĐã cài đặt thành công\e[0m"
 sleep 1
@@ -18,4 +22,4 @@ clear
 termux-wake-lock
 clear
 sleep 1
-exit
+login
